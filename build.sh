e3f65f5c68ade96d05211d535984e6c59bc5e117
Petro1990 <petrolobanok@gmail.com>
2026-04-01 19:59:11 +0300

    Исправление и восстановление интерфейса Mihomo Studio, дедупликация JavaScript кода, обновление до v2.1.8

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="2.1.7"
+VERSION="2.1.8"
 OUT_DIR="dist"
 
 mkdir -p "$OUT_DIR"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/l-ptrol/mhstudio-go/web"
 )
 
-var Version = "2.1.7"
+var Version = "2.1.8"
 
 func main() {
 	fStop := flag.Bool("stop", false, "Stop the server")
