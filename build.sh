9e2046f627e008eb3337ba8e297addc926c12f35
Petro1990 <petrolobanok@gmail.com>
2026-04-02 13:04:26 +0300

    Рефакторинг YAML-модалки добавления туннелей, фикс выпадающих списков, обновление версии v2.2.3 и настроенный .gitignore

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="2.2.2"
+VERSION="2.2.3"
 OUT_DIR="dist"
 
 mkdir -p "$OUT_DIR"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/l-ptrol/mhstudio-go/web"
 )
 
-var Version = "2.2.2"
+var Version = "2.2.3"
 
 func main() {
 	fStop := flag.Bool("stop", false, "Stop the server")
