43bc43b171031729deade628dd07c4eaa44e28b2
Petro1990 <petrolobanok@gmail.com>
2026-04-04 23:10:29 +0300

    v2.2.10: Исправление UI модального окна добавления прокси, адаптивность, отступы и логика вкладок

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="2.2.3"
+VERSION="2.2.10"
 OUT_DIR="dist"
 
 mkdir -p "$OUT_DIR"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/l-ptrol/mhstudio-go/web"
 )
 
-var Version = "2.2.3"
+var Version = "2.2.10"
 
 func main() {
 	fStop := flag.Bool("stop", false, "Stop the server")
