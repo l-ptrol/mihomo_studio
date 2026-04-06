433549ebe428a927d69b154ab37642abd0e652f9
Petro1990 <petrolobanok@gmail.com>
2026-04-06 22:15:55 +0300

    Исправление v2.2.20: тема/язык применяются до рендера страницы (inline script), tab-pane overflow-y:auto, editor overflow:hidden, min-height:0 для flex-цепочки

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="2.2.19"
+VERSION="2.2.20"
 OUT_DIR="dist"
 
 mkdir -p "$OUT_DIR"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/l-ptrol/mhstudio-go/web"
 )
 
-var Version = "2.2.19"
+var Version = "2.2.20"
 
 func main() {
 	fStop := flag.Bool("stop", false, "Stop the server")
