8c42ff312b15f39a600fd490a3fce59774ad17cd
Petro1990 <petrolobanok@gmail.com>
2026-04-06 22:58:51 +0300

    UI v2.2.26: восстановлены стили вкладок, добавлен gap:24px между контентом и сайдбаром, исправлена высота редактора через flex:1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="2.2.24"
+VERSION="2.2.26"
 OUT_DIR="dist"
 
 mkdir -p "$OUT_DIR"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/l-ptrol/mhstudio-go/web"
 )
 
-var Version = "2.2.24"
+var Version = "2.2.26"
 
 func main() {
 	fStop := flag.Bool("stop", false, "Stop the server")
