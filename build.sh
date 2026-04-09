dad70ad2e194e67ed1f639aae5daae0408aa9a66
Petro1990 <petrolobanok@gmail.com>
2026-04-09 15:53:32 +0300

    v2.2.82: исправление ошибок оболочки (LF) и настройка .gitattributes

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="2.2.81"
+VERSION="2.2.82"
 OUT_DIR="dist"
 
 mkdir -p "$OUT_DIR"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/l-ptrol/mhstudio-go/web"
 )
 
-var Version = "2.2.81"
+var Version = "2.2.82"
 
 func main() {
 	fStart := flag.Bool("start", false, "Start the server")
